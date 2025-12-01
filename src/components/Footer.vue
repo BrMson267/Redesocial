094f65354fc3dbaebc0ce5780a3a8e094b72816c
Bruno Martins Silva Oliveira do Nascimento <bruno.martinsson@outlook.com>
2025-12-01 15:52:16 -0300

    Primeiro commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <nav>
+        <div class ="contato">
+            <h3>Contato</h3>
+            <p>contato@inspiralens.com</p>
+            <p>Central de ajuda</p>
+            <p>Rio de Janeiro</p>
+        </div>
+        <div class="Sobre">
+            <p>Politicas</p>
+            <p>Politicas de privacidade</p>
+            <p>Blog</p>
+        </div>
+        <div class="inscrever">
+        <p>Assine nossa newletter</p>
+            
+            
+            <form>
+                <label for="email"></label>
+                <input type="email" id="email" placeholder="Digite seu email">
+                <button type="button">Inscrever-se</button>
+                
+            </form>
+            <div class="redesocial">
+                <img src="../assets/redesocial.svg" alt="Redes sociais">
+
+            </div>
+
+        </div>
+               
+
+    </nav>
+
+
+</template>
+
+
+<style scoped lang="scss"></style>
+
+    
+
